4024d6d1fe9940911b39392f0534dc346bc91e8e
Marius Küng <maku@mailbox.org>
2016-05-20 09:59:20 +0200

    Added react-google-maps with example maps

--- artmap/client/components/App.js
+++ artmap/client/components/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import Map from './Map';
 
 const App = React.createClass({
   render() {
+    let markers = [{
+      position: {
+        lat: 25.0112183,
+        lng: 121.52067570000001,
+      },
+      key: 'Taiwan',
+      defaultAnimation: 2,
+    }];
+
     return (
       <div>
         <h1>Artmap</h1>
+        <div className="map">
+          <Map markers={markers} />
+        </div>
       </div>
     )
   }
