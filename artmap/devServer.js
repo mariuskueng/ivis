69e9cd213936c9fb454302db4a4b5b5c1e0f47f3
Marius Küng <maku@mailbox.org>
2016-06-02 13:14:06 +0200

    Added artist url to search after artist names
    http://localhost:7770/artist?name=alberto%20giacometti

--- artmap/devServer.js
+++ artmap/devServer.js
@@ -2,6 +2,7 @@ var path = require('path');
 var express = require('express');
 var webpack = require('webpack');
 var config = require('./webpack.config.dev');
+var mysql      = require('mysql');
 
 var app = express();
 var compiler = webpack(config);
@@ -13,8 +14,39 @@ app.use(require('webpack-dev-middleware')(compiler, {
 
 app.use(require('webpack-hot-middleware')(compiler));
 
-app.get('*', function(req, res) {
+app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/artist', function(req, res) {
+  var queryString = 'SELECT RECNR, VORNAME, NAME, NAMENSSUCHE FROM Person WHERE';
+  var fullname = req.query.name.split(' ');
+
+  var connection = mysql.createConnection({
+    host     : 'server1102.cs.technik.fhnw.ch',
+    user     : 'ivis_fs16',
+    password : '',
+    database : 'ivis_fs16'
+  });
+
+  fullname.forEach(function(name) {
+    queryString += ' NAMENSSUCHE LIKE ' + connection.escape('%'+name+'%') + ' AND';
+  });
+
+  queryString = queryString.substring(0, queryString.length - 3);
+
+  try {
+    connection.connect();
+
+    connection.query(queryString, function(err, rows, fields) {
+      if (err) throw err;
+      res.send(rows);
+    });
+
+    connection.end();
+  } catch (e) {
+    res.send('SQL Error: ' + e);
+  }
 });
 
 app.listen(7770, 'localhost', function(err) {
