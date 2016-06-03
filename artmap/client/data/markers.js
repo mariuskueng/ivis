96cf3b1cfcda47029a6a15bcac80e7880ca5148e
Marius Küng <maku@mailbox.org>
2016-06-03 12:09:52 +0200

    Removed city from markers mocks

--- artmap/client/data/markers.js
+++ artmap/client/data/markers.js
@@ -5,14 +5,14 @@ const markers = [
           'lat': 51.6738583,
           'lng': 7.815981600000001
       },
-      'title': 'Städtisches Gustav-Lübcke-Museum Hamm',
+      'title': 'Städtisches Gustav-Lübcke-Museum',
       'defaultAnimation': 2
   }, {
       'position': {
           'lat': 51.3423734,
           'lng': 12.3757553
       },
-      'title': 'Museum der Bildenden Künste Leipzig',
+      'title': 'Museum der Bildenden Künste',
       'defaultAnimation': 2
   }, {
       'position': {
@@ -26,476 +26,476 @@ const markers = [
           'lat': 51.2277411,
           'lng': 6.7734556
       },
-      'title': 'Kunstsammlung Nordrhein-Westfalen Düsseldorf',
+      'title': 'Kunstsammlung Nordrhein-Westfalen',
       'defaultAnimation': 2
   }, {
       'position': {
           'lat': 48.782602,
           'lng': 9.187097699999999
       },
-      'title': 'Staatsgalerie Stuttgart ',
+      'title': 'Staatsgalerie',
       'defaultAnimation': 2
   }, {
       'position': {
           'lat': 47.2014207,
           'lng': 8.7777241
       },
-      'title': 'Seedamm-Kulturzentrum Pfäffikon (SZ)',
+      'title': 'Seedamm-Kulturzentrum',
       'defaultAnimation': 2
   }, {
       'position': {
           'lat': 47.5595986,
           'lng': 7.5885761
       },
-      'title': 'Öffentliche Kunstsammlung Basel, Kupferstichkabinett Basel',
+      'title': 'Öffentliche Kunstsammlung Basel, Kupferstichkabinett',
       'defaultAnimation': 2
   }, {
       'position': {
           'lat': 48.5734053,
           'lng': 7.752111299999999
       },
-      'title': 'Les Musées de la Ville de Strasbourg Strasbourg',
+      'title': 'Les Musées de la Ville de Strasbourg',
       'defaultAnimation': 2
   }, {
       'position': {
           'lat': 52.363873,
           'lng': 9.739173
       },
-      'title': 'Sprengel Museum Hannover',
+      'title': 'Sprengel Museum',
       'defaultAnimation': 2
   }, {
       'position': {
           'lat': 50.1109221,
           'lng': 8.6821267
       },
-      'title': 'Schirn Kunsthalle Frankfurt am Main',
+      'title': 'Schirn Kunsthalle',
       'defaultAnimation': 2
   }, {
       'position': {
           'lat': 53.8654673,
           'lng': 10.6865593
       },
-      'title': 'Kunsthalle St.Annen Lübeck',
+      'title': 'Kunsthalle St.Annen',
       'defaultAnimation': 2
   }, {
       'position': {
           'lat': 38.9071923,
           'lng': -77.0368707
       },
-      'title': 'The Phillips Collection Washington',
+      'title': 'The Phillips Collection',
       'defaultAnimation': 2
   }, {
       'position': {
           'lat': 40.7127837,
           'lng': -74.0059413
       },
-      'title': 'Neue Galerie. Museum for German and Austrian Art New York',
+      'title': 'Neue Galerie. Museum for German and Austrian Art',
       'defaultAnimation': 2
   }, {
       'position': {
           'lat': 29.7604267,
           'lng': -95.3698028
       },
-      'title': 'The Menil Collection Houston',
+      'title': 'The Menil Collection',
       'defaultAnimation': 2
   }, {
       'position': {
           'lat': 46.95080799999999,
           'lng': 7.472878
       },
-      'title': 'Zentrum Paul Klee Bern',
+      'title': 'Zentrum Paul Klee',
       'defaultAnimation': 2
   }, {
       'position': {
           'lat': 50.937531,
           'lng': 6.9602786
       },
-      'title': 'Museum Ludwig Köln',
+      'title': 'Museum Ludwig',
       'defaultAnimation': 2
   }, {
       'position': {
           'lat': 50.830441,
           'lng': 6.910565999999999
       },
-      'title': 'Max Ernst Museum Brühl',
+      'title': 'Max Ernst Museum',
       'defaultAnimation': 2
   }, {
       'position': {
           'lat': 49.24015720000001,
           'lng': 6.996932699999999
       },
-      'title': 'Saarlandmuseum. Moderne Galerie Saarbrücken',
+      'title': 'Saarlandmuseum. Moderne Galerie',
       'defaultAnimation': 2
   }, {
       'position': {
           'lat': 45.4654219,
           'lng': 9.1859243
       },
-      'title': 'Fondazione Antonio Mazzotta Milano',
+      'title': 'Fondazione Antonio Mazzotta',
       'defaultAnimation': 2
   }, {
       'position': {
           'lat': 47.80949,
           'lng': 13.05501
       },
-      'title': 'Museum der Moderne Salzburg',
+      'title': 'Museum der Moderne',
       'defaultAnimation': 2
   }, {
       'position': {
           'lat': 47.5595986,
           'lng': 7.5885761
       },
-      'title': 'Galerie Beyeler Basel',
+      'title': 'Galerie Beyeler',
       'defaultAnimation': 2
   }, {
       'position': {
           'lat': 48.20430589999999,
           'lng': 16.3690259
       },
-      'title': 'Albertina Wien',
+      'title': 'Albertina',
       'defaultAnimation': 2
   }, {
       'position': {
           'lat': 52.52000659999999,
           'lng': 13.404954
       },
-      'title': 'Neue Nationalgalerie, Staatliche Museen zu Berlin - Preussischer Kulturbesitz Berlin',
+      'title': 'Neue Nationalgalerie, Staatliche Museen zu Berlin - Preussischer Kulturbesitz',
       'defaultAnimation': 2
   }, {
       'position': {
           'lat': 49.3987524,
           'lng': 8.6724335
       },
-      'title': 'Heidelberger Kunstverein Heidelberg',
+      'title': 'Heidelberger Kunstverein',
       'defaultAnimation': 2
   }, {
       'position': {
           'lat': 46.4312213,
           'lng': 6.9106799
       },
-      'title': 'Maison Visinand Montreux',
+      'title': 'Maison Visinand',
       'defaultAnimation': 2
   }, {
       'position': {
           'lat': 41.9027835,
           'lng': 12.4963655
       },
-      'title': 'Galleria Nazionale d"Arte Moderna Roma',
+      'title': 'Galleria Nazionale d"Arte Moderna',
       'defaultAnimation': 2
   }, {
       'position': {
           'lat': 51.4556432,
           'lng': 7.0115552
       },
-      'title': 'Museum Folkwang Essen Essen',
+      'title': 'Museum Folkwang Essen',
       'defaultAnimation': 2
   }, {
       'position': {
           'lat': 53.5510846,
           'lng': 9.9936818
       },
-      'title': 'Hamburger Kunsthalle Hamburg',
+      'title': 'Hamburger Kunsthalle',
       'defaultAnimation': 2
   }, {
       'position': {
           'lat': 50.937531,
           'lng': 6.9602786
       },
-      'title': 'Museum für Ostasiatische Kunst Köln',
+      'title': 'Museum für Ostasiatische Kunst',
       'defaultAnimation': 2
   }, {
       'position': {
           'lat': 40.4167754,
           'lng': -3.7037902
       },
-      'title': 'Fundación Juan March Madrid',
+      'title': 'Fundación Juan March',
       'defaultAnimation': 2
   }, {
       'position': {
           'lat': 47.6775102,
           'lng': 11.2041398
       },
-      'title': 'Schlossmuseum Murnau Murnau',
+      'title': 'Schlossmuseum Murnau',
       'defaultAnimation': 2
   }, {
       'position': {
           'lat': 48.3705449,
           'lng': 10.89779
       },
-      'title': 'H2 - Zentrum für Gegenwartskunst im Glaspalast Augsburg',
+      'title': 'H2 - Zentrum für Gegenwartskunst im Glaspalast',
       'defaultAnimation': 2
   }, {
       'position': {
           'lat': 51.4556432,
           'lng': 7.0115552
       },
-      'title': 'E. & A. Silberman Galleries, inc. New York',
+      'title': 'E. & A. Silberman Galleries, inc.',
       'defaultAnimation': 2
   }, {
       'position': {
           'lat': 40.7127837,
           'lng': -74.0059413
       },
-      'title': 'Museum of Modern Art New York',
+      'title': 'Museum of Modern Art',
       'defaultAnimation': 2
   }, {
       'position': {
           'lat': 51.3423734,
           'lng': 12.3757553
       },
-      'title': 'Museum der bildenden Künste Leipzig Leipzig',
+      'title': 'Museum der bildenden Künste Leipzig',
       'defaultAnimation': 2
   }, {
       'position': {
           'lat': 51.507721,
           'lng': -0.0991941
       },
-      'title': 'Tate Modern London',
+      'title': 'Tate Modern',
       'defaultAnimation': 2
   }, {
       'position': {
           'lat': 51.2277411,
           'lng': 6.7734556
       },
-      'title': 'Kunstsammlung Nordrhein-Westfalen Düsseldorf',
+      'title': 'Kunstsammlung Nordrhein-Westfalen',
       'defaultAnimation': 2
   }, {
       'position': {
           'lat': 46.9509867,
           'lng': 7.443583400000001
       },
-      'title': 'Kunstmuseum Bern Bern',
+      'title': 'Kunstmuseum Bern',
       'defaultAnimation': 2
   }, {
       'position': {
           'lat': 41.9992618,
           'lng': 12.0999962
       },
-      'title': 'Palazzo Ruspoli Rom',
+      'title': 'Palazzo Ruspoli',
       'defaultAnimation': 2
   }, {
       'position': {
           'lat': 48.1351253,
           'lng': 11.5819806
       },
-      'title': 'Städtische Galerie  im Lenbachhaus München',
+      'title': 'Städtische Galerie  im Lenbachhaus',
       'defaultAnimation': 2
   }, {
       'position': {
           'lat': 52.52000659999999,
           'lng': 13.404954
       },
-      'title': 'Kupferstichkabinett, Berlin ',
+      'title': 'Kupferstichkabinett',
       'defaultAnimation': 2
   }, {
       'position': {
           'lat': 45.7349551,
           'lng': 7.313076199999999
       },
-      'title': 'Museo archeologico regionale Aosta',
+      'title': 'Museo archeologico regionale',
       'defaultAnimation': 2
   }, {
       'position': {
           'lat': 48.8595374,
           'lng': 2.3668699
       },
-      'title': 'Cité de la musique Paris',
+      'title': 'Cité de la musique',
       'defaultAnimation': 2
   }, {
       'position': {
           'lat': 40.7127837,
           'lng': -74.0059413
       },
-      'title': 'Buchholz Gallery C. Valentin New York',
+      'title': 'Buchholz Gallery C. Valentin',
       'defaultAnimation': 2
   }, {
       'position': {
           'lat': 50.8503396,
           'lng': 4.3517103
       },
-      'title': 'Palais des Beaux-Arts Bruxelles',
+      'title': 'Palais des Beaux-Arts',
       'defaultAnimation': 2
   }, {
       'position': {
           'lat': 46.95080799999999,
           'lng': 7.472878
       },
-      'title': 'Zentrum Paul Klee Bern',
+      'title': 'Zentrum Paul Klee',
       'defaultAnimation': 2
   }, {
       'position': {
           'lat': 53.5510846,
           'lng': 9.9936818
       },
-      'title': 'Hamburger Kunsthalle Hamburg',
+      'title': 'Hamburger Kunsthalle',
       'defaultAnimation': 2
   }, {
       'position': {
           'lat': 40.4167754,
           'lng': -3.7037902
       },
-      'title': 'Fundación Juan March Madrid',
+      'title': 'Fundación Juan March',
       'defaultAnimation': 2
   }, {
       'position': {
           'lat': 47.6775102,
           'lng': 11.2041398
       },
-      'title': 'Schlossmuseum Murnau Murnau',
+      'title': 'Schlossmuseum Murnau',
       'defaultAnimation': 2
   }, {
       'position': {
           'lat': 40.7127837,
           'lng': -74.0059413
       },
-      'title': 'Museum of Modern Art New York',
+      'title': 'Museum of Modern Art',
       'defaultAnimation': 2
   }, {
       'position': {
           'lat': 46.9509867,
           'lng': 7.443583400000001
       },
-      'title': 'Kunstmuseum Bern Bern',
+      'title': 'Kunstmuseum Bern',
       'defaultAnimation': 2
   }, {
       'position': {
           'lat': 46.95080799999999,
           'lng': 7.472878
       },
-      'title': 'Zentrum Paul Klee Bern',
+      'title': 'Zentrum Paul Klee',
       'defaultAnimation': 2
   }, {
       'position': {
           'lat': 53.5510846,
           'lng': 9.9936818
       },
-      'title': 'Hamburger Kunsthalle Hamburg',
+      'title': 'Hamburger Kunsthalle',
       'defaultAnimation': 2
   }, {
       'position': {
           'lat': 46.95080799999999,
           'lng': 7.472878
       },
-      'title': 'Zentrum Paul Klee Bern',
+      'title': 'Zentrum Paul Klee',
       'defaultAnimation': 2
   }, {
       'position': {
           'lat': 46.95080799999999,
           'lng': 7.472878
       },
-      'title': 'Zentrum Paul Klee Bern',
+      'title': 'Zentrum Paul Klee',
       'defaultAnimation': 2
   }, {
       'position': {
           'lat': 46.95080799999999,
           'lng': 7.472878
       },
-      'title': 'Zentrum Paul Klee Bern',
+      'title': 'Zentrum Paul Klee',
       'defaultAnimation': 2
   }, {
       'position': {
           'lat': 46.95080799999999,
           'lng': 7.472878
       },
-      'title': 'Zentrum Paul Klee Bern',
+      'title': 'Zentrum Paul Klee',
       'defaultAnimation': 2
   }, {
       'position': {
           'lat': 46.95080799999999,
           'lng': 7.472878
       },
-      'title': 'Zentrum Paul Klee Bern',
+      'title': 'Zentrum Paul Klee',
       'defaultAnimation': 2
   }, {
       'position': {
           'lat': 46.95080799999999,
           'lng': 7.472878
       },
-      'title': 'Zentrum Paul Klee Bern',
+      'title': 'Zentrum Paul Klee',
       'defaultAnimation': 2
   }, {
       'position': {
           'lat': 46.95080799999999,
           'lng': 7.472878
       },
-      'title': 'Zentrum Paul Klee Bern',
+      'title': 'Zentrum Paul Klee',
       'defaultAnimation': 2
   }, {
       'position': {
           'lat': 46.95080799999999,
           'lng': 7.472878
       },
-      'title': 'Zentrum Paul Klee Bern',
+      'title': 'Zentrum Paul Klee',
       'defaultAnimation': 2
   }, {
       'position': {
           'lat': 46.95080799999999,
           'lng': 7.472878
       },
-      'title': 'Zentrum Paul Klee Bern',
+      'title': 'Zentrum Paul Klee',
       'defaultAnimation': 2
   }, {
       'position': {
           'lat': 46.95080799999999,
           'lng': 7.472878
       },
-      'title': 'Zentrum Paul Klee Bern',
+      'title': 'Zentrum Paul Klee',
       'defaultAnimation': 2
   }, {
       'position': {
           'lat': 46.95080799999999,
           'lng': 7.472878
       },
-      'title': 'Zentrum Paul Klee Bern',
+      'title': 'Zentrum Paul Klee',
       'defaultAnimation': 2
   }, {
       'position': {
           'lat': 46.95080799999999,
           'lng': 7.472878
       },
-      'title': 'Zentrum Paul Klee Bern',
+      'title': 'Zentrum Paul Klee',
       'defaultAnimation': 2
   }, {
       'position': {
           'lat': 46.95080799999999,
           'lng': 7.472878
       },
-      'title': 'Zentrum Paul Klee Bern',
+      'title': 'Zentrum Paul Klee',
       'defaultAnimation': 2
   }, {
       'position': {
           'lat': 46.95080799999999,
           'lng': 7.472878
       },
-      'title': 'Zentrum Paul Klee Bern',
+      'title': 'Zentrum Paul Klee',
       'defaultAnimation': 2
   }, {
       'position': {
           'lat': 46.95080799999999,
           'lng': 7.472878
       },
-      'title': 'Zentrum Paul Klee Bern',
+      'title': 'Zentrum Paul Klee',
       'defaultAnimation': 2
   }, {
       'position': {
           'lat': 46.95080799999999,
           'lng': 7.472878
       },
-      'title': 'Zentrum Paul Klee Bern',
+      'title': 'Zentrum Paul Klee',
       'defaultAnimation': 2
   }, {
       'position': {
           'lat': 46.95080799999999,
           'lng': 7.472878
       },
-      'title': 'Zentrum Paul Klee Bern',
+      'title': 'Zentrum Paul Klee',
       'defaultAnimation': 2
   }, {
       'position': {
           'lat': 51.8222436,
           'lng': 12.2424516
       },
-      'title': 'Stiftung Bauhaus Dessau Dessau',
+      'title': 'Stiftung Bauhaus Dessau',
       'defaultAnimation': 2
   }
 ];
