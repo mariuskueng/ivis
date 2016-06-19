78e6326030baf083d91099a078c20836da3b5ae0
Marius Küng <maku@mailbox.org>
2016-06-19 22:37:14 +0200

    Added graph visualization

--- charts/main.js
+++ charts/main.js
@@ -1,19 +1,20 @@
 var data = [4, 8, 15, 16, 23, 42];
 
-// Scaling to Fit
-var x = d3.scale.linear()
-    .domain([0, d3.max(data)])
-    .range([0, 420]);
+d3.json("data4.json", function(error, data){
+  if(error) throw error;
+  var counts = data.map(function(obj) {
+    return obj.count;
+  }).sort(function(a, b){return a-b});
 
-d3.select(".chart")
-  .selectAll("div")
-    .data(data)
-  .enter().append("div")
-    .style("width", function(d) { return x(d) + "px"; })
-    .text(function(d) { return d; });
+  // Scaling to Fit
+  var x = d3.scale.linear()
+      .domain([0, d3.max(counts)])
+      .range([counts[0], counts[counts.length - 1]]);
 
-
-var artists = 2217;
-var artistWithMoreThan10Artworks = 672;
-
-// var circular = d3.scale
+  d3.select(".chart")
+    .selectAll("div")
+      .data(data)
+    .enter().append("div")
+      .style("width", function(d) { return x(d.count / 35) + "px"; })
+      .text(function(d) { return d.century + " (" + d.count + ")"; });
+});
